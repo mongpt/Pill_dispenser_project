a15dbcbdc31074c47668d03293eeab0d9a963899
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-19 12:03:55 +0200

    Update lorawan.h

--- lorawan.h
+++ lorawan.h
@@ -1,6 +1,6 @@
 #ifndef LORAWAN
 #define LORAWAN
 
-bool generic_lora(const char* command, const uint sleep_time, char* str);
+bool loraCommunication(const char* command, const uint sleep_time, char* str);
 
 #endif
