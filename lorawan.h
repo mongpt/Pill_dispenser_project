47d28339db50dc183adba58c44a189c5c9609513
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-20 10:44:48 +0200

    Update lorawan.h

--- lorawan.h
+++ lorawan.h
@@ -25,9 +25,49 @@ typedef struct lorawan_item_ {
     uint sleep_time;
 } lorawan_item;
 
+/**********************************************************************************************************************
+ * \brief: Initialises the uart and sets up lorawan communication
+ *
+ * \param: 
+ *
+ * \return: true: if connection established, false: if connection not established
+ *
+ * \remarks: Programmer should use this to initialize uart and lorawan communication.
+ **********************************************************************************************************************/
 bool loraInit();
+
+/**********************************************************************************************************************
+ * \brief: Communicates with uart.
+ *
+ * \param: 3 parameters. Takes the command to be sent, the sleep time to wait before read and the string to read the 
+ *         returned message to.
+ *
+ * \return: true: if uart responses, false: if uart does not response
+ *
+ * \remarks: Called by loraInit(), loraMsg(), retvalChecker(). Programmer should not use this function.
+ **********************************************************************************************************************/
 bool loraCommunication(const char* command, const uint sleep_time, char* str);
+
+/**********************************************************************************************************************
+ * \brief: Formats message and sends it to uart.
+ *
+ * \param: 3 parameters. Takes the message to be sent, message length and string where the returned message is read to.
+ *
+ * \return: true: if uart responses, false: if uart does not response
+ *
+ * \remarks: Programmer should use this to send message.
+ **********************************************************************************************************************/
 bool loraMsg(const char* message, size_t msg_size, char* return_message);
+
+/**********************************************************************************************************************
+ * \brief: Calls loraCommunication and compares the returned value with the value in case of success.
+ *
+ * \param: 1 parameter. Takes the index of the struct element.
+ *
+ * \return: true: if compared strings are the same, false: if compared strings are not the same
+ *
+ * \remarks: Called by loraInit(). Programmer should not use this function.
+ **********************************************************************************************************************/
 bool retvalChecker(const int index);
 
 #endif
